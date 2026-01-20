00ca317cabef8393ea87c411210bf9e4bea50995
project-3 <your-github-pruthvirajul2001@gmail.com@example.com>
2026-01-20 17:06:07 +0530

    bannii

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log("Server running on http://13.60.238.99:5000");
 });
